27f618211688d035d6bf35eac5c42a6b3d470e63
hyorish03 <hyorish2001@gmail.com>
2023-04-21 17:19:49 +0900

    #1.0 vanilla JS로 counter을 만들며 redux 기초 익히기

--- src/index.js
+++ src/index.js
@@ -3,25 +3,37 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("span");
 
+number.innerText = 0;
+
+const Add = "Add";
+const Minus = "Minus";
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    count++;
-  } else if (action.type === "Minus") {
-    count--;
-  } else return 0;
-  return count;
+  switch (action.type) {
+    case "Add":
+      return count + 1;
+    case "Minus":
+      return count - 1;
+    default:
+      return count;
+  }
 };
+
 const countStore = createStore(countModifier);
-console.log(countStore);
+
+console.log(countStore.getState());
+
 const onChange = () => {
+  number.innerText = countStore.getState();
   console.log("there was a change on the store");
 };
 countStore.subscribe(onChange);
+
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: Add });
 };
 
-const handleMinus = () => countStore.dispatch({ type: "Minus" });
+const handleMinus = () => countStore.dispatch({ type: Minus });
+
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
